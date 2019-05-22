86968025e848b52885d12e65f6de5d32ed57849e
enzhe lu <lue3@rpi.edu>
2019-05-22 18:29:47 -0400

    Update first 6 projects

---
+++ 6.Xylophone/Xylophone/ VIewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var audioPlayer : AVAudioPlayer!
+    let SoundArray = ["note1","note2","note3","note4","note5","note6","note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        playSound(sendTag : sender.tag)
+
+    }
+    
+    func playSound(sendTag : Int){
+        
+        let soundURL = Bundle.main.url(forResource: SoundArray[sendTag-1], withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+        
+    }
+    
+  
+
+}
+
